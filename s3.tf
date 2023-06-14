9ddbda41ca7a7d19a2f4334b79bbac580d197522
Lantrix <lantrix@pobox.com>
2023-06-14 22:51:03 +1000

    Add S3 bucket

---
+++ s3.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "techdebug" {
+  bucket = var.bucketName
+}
+
+resource "aws_s3_bucket_public_access_block" "techdebug-public" {
+  bucket = aws_s3_bucket.techdebug.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "techdebug-config" {
+  bucket = aws_s3_bucket.techdebug.bucket
+  index_document = {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "techdebug-policy" {
+  bucket = aws_s3_bucket.techdebug.id
+  policy = templatefile("${path.module}/s3-policy.json", { bucket = var.bucketName })
+}
