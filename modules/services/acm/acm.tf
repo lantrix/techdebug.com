6b87fa02047dbb4f3e7c8b5ad632e546704707de
Lantrix <lantrix@pobox.com>
2023-06-18 19:12:07 +1000

    Add implicit declaration

--- modules/services/acm/acm.tf
+++ modules/services/acm/acm.tf
@@ -3,6 +3,14 @@
 variable "region" {}
 variable "domain" {}
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.3.0"
+    }
+  }
+}
 data "aws_route53_zone" "techdebug" {
   name = "${var.domain}."
 }
